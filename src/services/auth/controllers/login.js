6a04e348c2275670f7258f64c1d1d5b7b9b3b8f5
PabloPlaceres <pabloplacere@gmail>
2024-03-19 23:27:45 -0400

    Arregle el error del login

--- src/services/auth/controllers/login.js
+++ src/services/auth/controllers/login.js
@@ -10,7 +10,7 @@ export const login = async(req = request, res = response)=>{
     const {usuario, password} = req.body
     try {
         let user = await loginQuery(usuario)
-        if(!user){res.status(400).json({msg: 'No se ha econtrado un usuario con esos datos'})}
+        if(!user){return res.status(400).json({msg: 'No se ha econtrado un usuario con esos datos'})}
         
 
         const verifiPassword = bcryptjs.compareSync(password, user.password)
