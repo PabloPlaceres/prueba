0c2a1074f5bdaae9a0da77024a867199811ecacb
Pablo <142744413+PabloPlaceres@users.noreply.github.com>
2024-07-05 03:48:26 -0400

    ahora lo mismo desde prueba

--- src/limpiarDB.js
+++ src/limpiarDB.js
@@ -1,4 +1,4 @@
-/*import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient()
 
@@ -10,4 +10,4 @@ const eliminarTodo = async ()=>{
    await prisma.paquete.deleteMany()
    await prisma.repartidor.deleteMany()
    await prisma.cliente.deleteMany() 
-}*/
+}
